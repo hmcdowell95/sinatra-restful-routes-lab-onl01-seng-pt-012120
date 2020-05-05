581ac17d27f566252dd3c63461c59230772c6dd5
Harley McDowell <hmcdowell95@gmail.com>
2020-05-05 01:16:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,47 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  post '/recipes/new' do
+    @recipe = Recipe.create(params)
+    if @recipe
+      @id = @recipe.id
+      redirect "/recipes/#{@id}"
+    end
+    redirect "/recipes/new"
+  end
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+  
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  patch 'recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params)
+    erb :show
+  end
+  
+  delete '/recipes/:id' do 
+    Recipe.destroy(params[:id])
+    erb :index
+  end
 
 end
